75fb25b6350fc243fd6fa37c94822db716b1048d
Steven Ngo <stevenngo97@gmail.com>
2021-08-05 16:36:15 +1000

    Optimize data processing

--- data.py
+++ data.py
@@ -1,29 +1,37 @@
 import pandas as pd
-pd.options.mode.chained_assignment = None  # default='warn'
+from typing import Tuple
+import calendar
+
+#Turn off pandas warning
+pd.options.mode.chained_assignment = None 
 
 def get_json_reponse(file_name: str) -> dict:
     json_output = []
     df = cleanse_data(file_name)
-    count_df, lga_test_df = process_data(df)[0], process_data(df)[1]
+    df_tuple = process_data(df)
+    count_df, lga_test_df = df_tuple[0], df_tuple[1]
 
     for _, row in count_df.iterrows():
         lga_code = row['lga_code19']
         lga_name = row['lga_name19']
         lga_total_test_count = row['count']
+        
         # Get df of current lga's test count for each test dates
         curr_lga_df = lga_test_df.loc[lga_test_df['lga_code19'] == lga_code]
-        
+        lga_greatest_day_data = get_day_with_least_test(curr_lga_df)
+        lga_least_day_data = get_day_with_least_test(curr_lga_df)
+
         json_output.append({
             'lga_code': lga_code,
             'lga_name': lga_name,
             'total_count': lga_total_test_count,
             'greatest': {
-                'count': get_day_with_most_test(curr_lga_df)[0],
-                'date': get_day_with_most_test(curr_lga_df)[1]
+                'count': lga_greatest_day_data[0],
+                'date': lga_greatest_day_data[1]
             },
             'least': {
-                'count': get_day_with_least_test(curr_lga_df)[0],
-                'date': get_day_with_least_test(curr_lga_df)[1]
+                'count': lga_least_day_data[0],
+                'date': lga_least_day_data[1]
             },
         })
 
@@ -38,7 +46,7 @@ def process_data(df: pd.DataFrame):
     # Test date count group by lga ans test date DataFrame
     test_date_count_df = df.groupby(['lga_code19','test_date']).size().to_frame('count').reset_index()
  
-    return lga_count_df, test_date_count_df
+    return (lga_count_df, test_date_count_df)
 
 
 def cleanse_data(file_name: str) -> pd.DataFrame: 
@@ -55,11 +63,23 @@ def cleanse_data(file_name: str) -> pd.DataFrame:
     return df_not_null
 
 
-def get_day_with_most_test(input_df: pd.DataFrame):
+def format_date_ddmonyyyy(date_str :str):
+    """Format YYYY-MM-DD date to DD-Mon-YYYY"""
+    day = date_str.rsplit("-")[2]
+    year = date_str.rsplit("-")[0]
+    month_int = int(date_str.rsplit("-")[1])
+    month = calendar.month_abbr[month_int]
+
+    return day + '-' + month + '-' + year
+
+
+def get_day_with_most_test(input_df: pd.DataFrame) -> Tuple:
     df = input_df.loc[input_df['count'].idxmax()]
-    return int(df['count']), df['test_date']
+    formatted_date = format_date_ddmonyyyy(df['test_date'])
+    return (int(df['count']), formatted_date)
 
 
-def get_day_with_least_test(input_df: pd.DataFrame):
+def get_day_with_least_test(input_df: pd.DataFrame) -> Tuple:
     df = input_df.loc[input_df['count'].idxmin()]
-    return int(df['count']), df['test_date']
+    formatted_date = format_date_ddmonyyyy(df['test_date'])
+    return (int(df['count']), formatted_date)
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
-from flask import request, Blueprint, render_template, redirect, url_for
 import os.path
-from json import dumps
 
+from flask import request, Blueprint, render_template, redirect, url_for
+from json import dumps
 from werkzeug.utils import redirect
 
 from data import get_json_reponse
@@ -9,32 +9,16 @@ from data import get_json_reponse
 
 routes = Blueprint('routes', __name__)
 
-# Upload folder
-UPLOAD_FOLDER = 'csv'
-
-
-@routes.route('/', methods=['GET', 'POST'])
-def homepage():
-    if request.method == 'POST':
-        file = request.files['file']
-        try:
-            file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-            return redirect(url_for('routes.get_data', csv_file=file.filename))
-        except FileNotFoundError:
-            return render_template("index.html")
-    return render_template('index.html')
-
 
 @routes.route('/data/api/<csv_file>', methods=['GET'])
 def get_data(csv_file):
     if request.method == 'GET':
         requesting_csv_file = './csv/{}'.format(csv_file)
-
-        if os.path.isfile(requesting_csv_file):
+        try:
             output = get_json_reponse(requesting_csv_file)
             return dumps(output)
-        else:
-            return dumps({
+        except FileNotFoundError:
+            return dumps ({
                 'status_code': 404,
-                'error_status': 'file not found'
+                'error_status' : 'File not found'
             })
